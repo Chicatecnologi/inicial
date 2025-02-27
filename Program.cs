60b8d6fa0828bddecf0309eda2331d29bf3dbbcd
Chicatecnologi <mariafernandareyesmedina92@gmail.com>
2025-02-27 18:29:35 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Introduce tu nombre: ");
+        string nombre = Console.ReadLine();
+
+        if (!string.IsNullOrWhiteSpace(nombre))
+        {
+            char inicial = nombre.Trim()[0];
+            Console.WriteLine($"Tu inicial es: {inicial}");
+        }
+        else
+        {
+            Console.WriteLine("No ingresaste un nombre válido.");
+        }
+    }
+}
